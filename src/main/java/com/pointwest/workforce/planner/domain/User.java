040d1b2088c337b8424ae216be465802e71938db
Bryan Batanes <bryan.batanes@pointwest.com.ph>
2017-04-06 21:41:14 +0800

    added controllers

--- src/main/java/com/pointwest/workforce/planner/domain/User.java
+++ src/main/java/com/pointwest/workforce/planner/domain/User.java
@@ -22,4 +22,30 @@ public class User {
 	
 	@Column(name="employee_id")
 	private int employeeId;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSystemRole() {
+		return systemRole;
+	}
+
+	public void setSystemRole(String systemRole) {
+		this.systemRole = systemRole;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	
 }
