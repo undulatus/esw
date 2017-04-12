7f12053ef446555a8804b8249d2cad152c69fde1
bryan.batanes <bryan.batanes>
2017-04-12 15:49:47 +0800

    activity removed unused

--- src/main/java/com/pointwest/workforce/planner/controller/ActivityController.java
+++ src/main/java/com/pointwest/workforce/planner/controller/ActivityController.java
@@ -4,13 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pointwest.workforce.planner.domain.Activity;
-import com.pointwest.workforce.planner.domain.ServiceLine;
 import com.pointwest.workforce.planner.service.ReferenceDataService;
 import com.pointwest.workforce.planner.service.TemplateDataService;
 
