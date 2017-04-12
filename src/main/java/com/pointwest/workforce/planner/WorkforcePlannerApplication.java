b8f49bb48d80929848f6d18f729e4c1ebfc3305a
bryan.batanes <bryan.batanes>
2017-04-12 11:21:13 +0800

    update by egi

--- src/main/java/com/pointwest/workforce/planner/WorkforcePlannerApplication.java
+++ src/main/java/com/pointwest/workforce/planner/WorkforcePlannerApplication.java
@@ -2,15 +2,17 @@ package com.pointwest.workforce.planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 
 @PropertySource("classpath:db-values.properties")
 @SpringBootApplication
-public class WorkforcePlannerApplication {
+public class WorkforcePlannerApplication extends SpringBootServletInitializer {
 	
 	/**
      * Added for parsing
@@ -26,4 +28,9 @@ public class WorkforcePlannerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WorkforcePlannerApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WorkforcePlannerApplication.class);
+    }
 }
